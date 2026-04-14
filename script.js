a0a545aadbbc1adb617fd6c4a1671d8535f8e356
Ahmad Iqbal <iqbalgamez18@gmail.com>
2026-04-15 03:06:26 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// LOADER
+window.addEventListener("load", () => {
+  document.getElementById("loader").style.display = "none";
+});
+
+// SCROLL ANIMATION
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+  reveals.forEach(el => {
+    const top = el.getBoundingClientRect().top;
+    if (top < window.innerHeight - 100) {
+      el.classList.add("active");
+    }
+  });
+});
+
